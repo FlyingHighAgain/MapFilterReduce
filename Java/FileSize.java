f36d2da7648249bcc7cb767a85596131abebd1b3
FlyingHighAgain <tdnr.sym@gmail.com>
2014-07-12 14:56:21 +0900

    Reformat code

--- Java/FileSize.java
+++ Java/FileSize.java
@@ -3,31 +3,36 @@
 // サイズ情報用のクラス
 public class FileSize {
 
-  public FileSize(String fileName, long size) {
-    this.fileName = fileName;
-    this.size = size;
-  }
+    public FileSize(String fileName, long size) {
+        this.fileName = fileName;
+        this.size = size;
+    }
 
-  private String fileName;
-  public String getFileName() { return this.fileName; }
+    private String fileName;
 
-  private long size;
-  public long getSize() { return this.size; }
+    public String getFileName() {
+        return this.fileName;
+    }
 
-	public static FileSize parse(String line)	{
-		String[] items = line.split ("\t");
-		if (items.length < 2)
-		{
-			return null;
-		}
-		
-		return new FileSize(items[0], Long.valueOf(items[1]));
-	}
+    private long size;
 
-  @Override
-	public String toString () 	{
-		return String.format("[FileSize: FileName=%s, Size=%d]", fileName, size);
-	}
+    public long getSize() {
+        return this.size;
+    }
+
+    public static FileSize parse(String line) {
+        String[] items = line.split("\t");
+        if (items.length < 2) {
+            return null;
+        }
+
+        return new FileSize(items[0], Long.valueOf(items[1]));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[FileSize: FileName=%s, Size=%d]", fileName, size);
+    }
 
 }
 
--- Java/MapFilterReduce.java
+++ Java/MapFilterReduce.java
@@ -9,58 +9,60 @@
   サイズを集約する(Reduce)
 */
 
-import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 class MapFilterReduce {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	    // ファイルを読み込みファイル名とサイズの一覧を取得
+        // ファイルを読み込みファイル名とサイズの一覧を取得
         List<FileSize> fileSizes = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("filelist.txt"))) {
 
-          String line;
-          while((line = br.readLine()) != null) {
+            String line;
+            while ((line = br.readLine()) != null) {
 
-            FileSize fs = FileSize.parse(line);
-            if (fs != null) {
-              fileSizes.add(fs);
+                FileSize fs = FileSize.parse(line);
+                if (fs != null) {
+                    fileSizes.add(fs);
+                }
             }
-          }
 
         } catch (IOException e) {
-          System.out.println(e.toString());
+            System.out.println(e.toString());
         }
         //printList(fileSizes);
 
         // ファイル名がAで始まるデータを抽出(Filter)
-        List<FileSize> filteredList = fileSizes.stream()
-	        .filter(fs -> fs.getFileName().toUpperCase().startsWith("A"))
-            .collect(Collectors.toList());
+        List<FileSize> filteredList = fileSizes
+                .stream()
+                .filter(fs -> fs.getFileName().toUpperCase().startsWith("A"))
+                .collect(Collectors.toList());
         printList(filteredList);
 
 
-	    // サイズデータだけに変換する(Map)
-	    List<Long> sizes = filteredList.stream()
-		    .map(fs -> fs.getSize())
-            .collect(Collectors.toList());
-	    printList(sizes);
+        // サイズデータだけに変換する(Map)
+        List<Long> sizes = filteredList
+                .stream()
+                .map(fs -> fs.getSize())
+                .collect(Collectors.toList());
+        printList(sizes);
 
-	    // 合計サイズを計算する(Reduce)
-	    long totalSize = sizes.stream()
-            .reduce(0L, (total, next) -> total+next);
-	    System.out.println("total size: " + totalSize);
+        // 合計サイズを計算する(Reduce)
+        long totalSize = sizes.stream()
+                .reduce(0L, (total, next) -> total + next);
+        System.out.println("total size: " + totalSize);
 
-	}
+    }
 
-	public static <T> void printList(List<T> list) 	{
-		String s = "[" + String.join(",", list.stream().map(item -> item.toString()).toArray(String[]::new)) + "]";
-		System.out.println(s);
-	}
+    public static <T> void printList(List<T> list) {
+        String s = "[" + String.join(",", list.stream().map(item -> item.toString()).toArray(String[]::new)) + "]";
+        System.out.println(s);
+    }
 
 }
