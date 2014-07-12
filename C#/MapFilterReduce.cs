049d30d5b2909513c49157928fbf9bcaae14f087
FlyingHighAgain <tdnr.sym@gmail.com>
2014-07-12 15:35:54 +0900

    Reformat code

--- C#/MapFilterReduce.cs
+++ C#/MapFilterReduce.cs
@@ -8,7 +8,6 @@
   サイズデータだけに変換(Map)
   サイズを集約する(Reduce)
 */
-
 using System;
 using System.IO;
 using System.Linq;
@@ -16,34 +15,34 @@ using System.Collections.Generic;
 
 class MapFilterReduce
 {
-	public static void Main (string[] args)
+	public static void Main(string[] args)
 	{
 		// ファイルを読み込みファイル名とサイズの一覧を取得
 		var fileSizes = File.ReadLines("filelist.txt")
-			.Select (line => FileSize.Parse(line))
-			.Where (fs => fs != null);
+			.Select(line => FileSize.Parse(line))
+			.Where(fs => fs != null);
 		
 		// ファイル名がAで始まるデータを抽出(Filter)
 		var filteredList = fileSizes
-			.Where (fs => fs.FileName.ToUpper().StartsWith("A"))
+			.Where(fs => fs.FileName.ToUpper().StartsWith("A"))
 			.ToList();
 		PrintList(filteredList);
 		
 		// サイズデータだけに変換する(Map)
 		var sizes = filteredList
 			.Select(fs => fs.Size)
-			.ToList ();
+			.ToList();
 		PrintList(sizes);
 		
 		// 合計サイズを計算する(Reduce)
-		var totalSize = sizes.Aggregate(0L, (total, next) => total+next);
+		var totalSize = sizes.Aggregate(0L, (total, next) => total + next);
 		Console.WriteLine("total size: {0}", totalSize);
 		
 	}
 
 	public static void PrintList<T>(IEnumerable<T> list)
 	{
-		string s = "[" + string.Join (",", list.Select(item => item.ToString()).ToArray()) + "]";
+		string s = "[" + string.Join(",", list.Select(item => item.ToString()).ToArray()) + "]";
 		Console.WriteLine(s);
 	}
 	
--- C#/MapFilterReduce2.cs
+++ C#/MapFilterReduce2.cs
@@ -7,7 +7,6 @@
   ファイルタイプ（拡張子）毎の合計サイズを計算し出力する。
   ただし、拡張子のないデータは除く
 */
-
 using System;
 using System.IO;
 using System.Linq;
@@ -15,17 +14,17 @@ using System.Collections.Generic;
 
 class MapFilterReduce2
 {
-	public static void Main (string[] args)
+	public static void Main(string[] args)
 	{
 		// ファイルを読み込みファイル名とサイズの一覧を取得
 		var fileSizes = File.ReadLines("filelist.txt")
-			.Select (line => FileSize.Parse(line))
-			.Where (fs => fs != null);
+			.Select(line => FileSize.Parse(line))
+			.Where(fs => fs != null);
 		
 		// ファイル名を拡張子に変換（Map）
 		var fileExtSizes = fileSizes
 			.Select(fs => 
-			       new FileSize()
+			       new FileSize ()
 			       {
 						FileName = Path.GetExtension(fs.FileName).ToUpper(),
 						Size = fs.Size
@@ -35,7 +34,7 @@ class MapFilterReduce2
 		
 		// 拡張子のないデータを削除(Filter)
 		var fileExtSizes2 = fileExtSizes
-			.Where (fs => !string.IsNullOrWhiteSpace(fs.FileName))
+			.Where(fs => !string.IsNullOrWhiteSpace(fs.FileName))
 			.ToList();
 		//PrintList(fileExtSizes2);
 		
@@ -47,12 +46,12 @@ class MapFilterReduce2
 		
 		// グループ毎にサイズを集約（Reduce）
 		var extSizes = extGroups
-			.Select (grp => 
-			        new FileSize() {
+			.Select(grp => 
+			        new FileSize () {
 						FileName = grp.Key,
 						Size = grp.Sum(fs => fs.Size)
 					})
-			.ToList ();
+			.ToList();
 		//PrintList(extSizes);
 		
 		// サイズの大きい順にソート
@@ -60,15 +59,14 @@ class MapFilterReduce2
 			.OrderByDescending(fs => fs.Size)
 			.ToList();
 		//PrintList(extSize2);
-		foreach (FileSize fs in extSize2)
-		{
+		foreach (FileSize fs in extSize2) {
 			Console.WriteLine("{0}\t{1}", fs.FileName, fs.Size);	
 		}
 	}
 
 	public static void PrintList<T>(IEnumerable<T> list)
 	{
-		string s = "[" + string.Join (",", list.Select(item => item.ToString()).ToArray()) + "]";
+		string s = "[" + string.Join(",", list.Select(item => item.ToString()).ToArray()) + "]";
 		Console.WriteLine(s);
 	}
 	
